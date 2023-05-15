0a2c6d833cc274ed91e7ba4c9819d51382c03a0a
Moni97sha-S <129970757+Moni97sha-S@users.noreply.github.com>
2023-05-16 00:21:31 +0530

    updates js

---
+++ JS/05-15-js-intro/script.js
@@ -0,0 +1,71 @@
+//External Javascript
+//alert("Hi, how are you?");
+//console.log(1 + 3);
+/* DDDDFsdwf */
+//console.log(657 + 100);
+
+// container
+/* var name = 'Monisha';
+console.log(name); */
+
+//close container
+/* var engMark = 95;
+
+let engMark = {};
+var mathMark = 95;
+var engMark = 'wwww';
+var socialMark = [];
+var tamilMark = true;
+
+var total = engMark + mathMark + tamilMark + socialMark;
+console.log(total); */
+
+/* var name = 'abc';
+var name = '123';
+console.log(name); */
+// result: 123
+
+/* let mark = 100;
+let mark = 90;
+console.log(mark);
+//res: 100
+{
+    let mark = 90;
+    console.log(mark); 
+    // res: 80
+}
+console.log(mark);
+//res: 100
+*/
+
+/* let mark = 100;
+mark = 90;
+console.log(mark);
+//res: 90
+{
+    let mark = 80;
+    console.log(mark);
+    // res: 80
+    {
+        let mark = 70;
+        console.log(mark);
+        // res: 70
+    }
+}
+console.log(mark);
+// res: 90; As it will never consider mark inside a block, they are private.   */
+
+/*
+{
+let noofbook = 3;
+noofbook = 4;
+}
+{
+    console.log(noofbook);
+    // This results in error as 2 different blocks consider as private.
+}
+*/
+
+const pi = 3.14;
+pi = 4;
+// TypeError: Assignment to constant variable.
